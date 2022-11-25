f736d600e277061e2bd920af0fc690892d942a54
Anna Merkulova <panechek@inbox.ru>
2022-11-26 00:34:03 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import List from "./components/List";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="todo">
+      <div className="todo__sidebar">
+        <List items={[
+          {
+            icon:  (<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-list" viewBox="0 0 16 16">
+            <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
+          </svg>),
+          name: "Все задачи",
+          active: true,
+          },
+        ]} />
+        <List items={[
+          {
+            color: 'green',
+            name: "Покупки",
+          },
+          {
+            color: 'blue',
+            name: "Уборка",
+          },
+          {
+            color: 'pink',
+            name: "Учеба",
+          },
+        ]} />
+      </div>
+      <div className="todo__tasks"></div>
     </div>
   );
 }
